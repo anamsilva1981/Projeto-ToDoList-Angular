150b591355e3ff1a12894d3ec5893e4f5a90c4a8
anamsilva1981 <ana.msilva@hotmail.com>
2024-07-20 17:13:55 -0300

    :star: Commit inicial + criacao das pastas iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/home/components/header/header.component';
+import { TodoButtonDeleAllComponent } from './modules/home/components/todo-button-dele-all/todo-button-dele-all.component';
+import { TodoInputAddItensComponent } from './modules/home/components/todo-input-add-itens/todo-input-add-itens.component';
+import { TodoListComponent } from './modules/home/components/todo-list/todo-list.component';
+
+//Pages
+import { HomeComponent } from './modules/home/pages/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoButtonDeleAllComponent, 
+    TodoInputAddItensComponent, 
+    TodoListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeleAllComponent } from './components/todo-button-dele-all/todo-button-dele-all.component';
+import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    TodoButtonDeleAllComponent,
+    TodoInputAddItensComponent,
+    TodoListComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
